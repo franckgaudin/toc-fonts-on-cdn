a89b036958ac4f40dc2f7df14bd2326f3965570c
Franck Gaudin <franck.gaudin@workleap.com>
2023-12-08 08:43:48 -0500

    add tailwind and build docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react()],
+    optimizeDeps: {
+      include: ['react/jsx-runtime'],
+    },
+    plugins: [react({
+      jsxRuntime: "automatic"
+    })],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        react: path.resolve(__dirname, "node_modules/react"),
+      }
+    },
   }
 
   if (command === 'build') {
